b6f1fadbe01de83f7bec033e2a80378074744074
Enmanuel D Becerra C <becerracenmanueld@gmail.com>
2025-11-14 11:10:02 -0400

    📌 Nodepp-SQLite | Stable Release | V1.0.0 📌

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,5 @@
 #include <nodepp/nodepp.h>
-#include <sqlite.h>
+#include <sqlite/sqlite.h>
 
 using namespace nodepp;
 
@@ -7,38 +7,51 @@ void onMain() {
 
     sqlite_t db ("myDB.db");
 
-    db.exec(R"(
+    db.resolve(R"(
         CREATE TABLE COMPANY(
         ID INT PRIMARY KEY     NOT NULL,
         NAME           TEXT    NOT NULL,
         AGE            INT     NOT NULL,
         ADDRESS        CHAR(50),
         SALARY         REAL );
-    )");
+    )").await();
 
-    db.exec(R"(
+    db.resolve(R"(
         INSERT INTO COMPANY ( ID, NAME, AGE, ADDRESS, SALARY )
         VALUES (1, 'Paul', 32, 'California', 20000.00 );
-    )");
+    )").await();
 
-    db.exec(R"(
+    db.resolve(R"(
         INSERT INTO COMPANY ( ID, NAME, AGE, ADDRESS, SALARY )
         VALUES (2, 'John', 32, 'California', 20000.00 );
-    )");
+    )").await();
 
-    db.exec(R"(
+    db.resolve(R"(
         INSERT INTO COMPANY ( ID, NAME, AGE, ADDRESS, SALARY )
         VALUES (3, 'Mery', 32, 'California', 20000.00 );
-    )");
+    )").await();
 
-    db.exec(R"(
+    db.resolve(R"(
         INSERT INTO COMPANY ( ID, NAME, AGE, ADDRESS, SALARY )
         VALUES (4, 'Pipi', 32, 'California', 20000.00 );
-    )");
+    )").await();
 
-    db.exec("SELECT * from COMPANY",[]( object_t args ){
+    db.resolve(R"(
+        INSERT INTO COMPANY ( ID, NAME, AGE, ADDRESS, SALARY )
+        VALUES (4, 'Pipi', 32, 'California', 20000.00 );
+    )")
+
+    .then([=]( array_t<sql_item_t> data ){
+        // data
+    })
+
+    .fail([=]( except_t err ){
+        //err
+    });
+
+    db.emit("SELECT * from COMPANY",[]( sql_item_t args ){
         for( auto &x: args.keys() ){
-             console::log( x, "->", args[x].as<string_t>() );
+             console::log( x, "->", args[x] );
         }
     });
 
